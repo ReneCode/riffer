8b18eabcf5f8fc709368370565ea1fe69ac16a19
rene <harryloveslearning@googlemail.com>
2015-12-19 16:16:23 +0100

    current version

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,32 @@
 
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var data = '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">' +
+           '<foreignObject width="100%" height="100%">' +
+           '<div xmlns="http://www.w3.org/1999/xhtml" style="font-size:40px">' +
+             '<em>I</em> like ' + 
+             '<span style="color:white; text-shadow:0 0 2px blue;">' +
+             'cheese</span>' +
+           '</div>' +
+           '</foreignObject>' +
+           '</svg>';
+
+var DOMURL = window.URL || window.webkitURL || window;
+
+var img = new Image();
+var svg = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+var url = DOMURL.createObjectURL(svg);
+
+img.onload = function () {
+  ctx.drawImage(img, 0, 0);
+  DOMURL.revokeObjectURL(url);
+}
+
+img.src = url;
+
+/*
+
 var Riffer = (function() {
 
 	this.getChats = function() {
@@ -19,3 +47,4 @@ $(document).ready(function() {
 		$("#chatlist").append("<div>" + chat.name + "</div>");
 	})
 });
+*/
