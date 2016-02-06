8281f24624ab936191cb5926a8df5ad7552311aa
HarryMiller <HarryMillerMail@googlemail.com>
2016-02-06 23:50:13 +0100

    change getApiHost()

--- javascript/utility.js
+++ javascript/utility.js
@@ -1,25 +1,26 @@
 
 
-function getApiHost() {
+function _getApiHost() {
 	var apiHost = "http://localhost:64020";
 	return apiHost;
 }
 
-	/*
-	var host = $location.host();
-	if (host != 'localhost') {
+function getApiHost() {
+	var apiHost = "http://localhost:64020";
+	var host = location.host;
+	if (!host.match('localhost.*')) {
 	  // change route to subdomain "<protocol>://api.<host>"
 	  // =>   different service / look at .htaccess !
 	  // remove the subdomain
 	  host = host.match(/[^\.]*\.[^.]*$/)[0];
-	  apiHost = $location.protocol() + "://api." + host;
-	  var port = $location.port();
+	  apiHost = location.protocol + "://rifferapi." + host;
+/*	  var port = location.port;
 	  if (port) {
 	    apiHost += ":" + port;
-	  }
+	  } */
 	}
 	return apiHost;
-	},
-*/
+	}
+
 
 exports.getApiHost = getApiHost;
